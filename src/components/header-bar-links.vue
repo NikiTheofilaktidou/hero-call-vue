da96cde5a445ef24da5c3cf39ac3d2fd9701ad06
Niki <nikitheofilaktidou@gmail.com>
2022-06-30 16:41:36 +0300

    github input

--- src/components/header-bar-links.vue
+++ src/components/header-bar-links.vue
@@ -4,7 +4,13 @@
       <div class="navbar-item">
         <div class="buttons">
           <a :href="github" target="_blank" rel="noopener noreferrer">
+           <label class="label" for="github">Github</label>
+            <input type="text" id="github" class="input" v-model="github">
+             {{ github }}
             <i class="fab fa-github fa-2x" aria-hidden="true"></i>
+            <div class="field">
+            
+             </div>
           </a>
           <a :href="twitter" target="_blank" rel="noopener noreferrer">
             <i class="fab fa-twitter fa-2x" aria-hidden="true"></i>
